c0d92b4b2b10a462219b92c27d1a50fca1174360
raduga9199 <raduga9199@gmail.com>
2023-06-28 12:19:08 -0400

    dto

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,8 +1,26 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Data
 public class User extends BaseEntity {
+
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String password, boolean enable, String phone, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.enable = enable;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
 
     private String firstName;
     private String lastName;
