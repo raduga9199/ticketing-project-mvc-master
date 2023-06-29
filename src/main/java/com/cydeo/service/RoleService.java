5d2176834fe8c0c7bcfa0a92d5ede18c6990db61
raduga9199 <raduga9199@gmail.com>
2023-06-29 08:59:57 -0400

    service implementation

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -2,24 +2,9 @@ package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
 
-import java.util.List;
 
-public interface RoleService extends CrudService<RoleDTO,String>{
+public interface RoleService extends CrudService<RoleDTO,Long>{
 
-    // Save
-    // Find by
-    // Find All
-    // Edit
-    // Update
-    // Delete
-/*
-    RoleDTO save(RoleDTO role);
-    RoleDTO findByID(String username);
-    List<RoleDTO> findAll();
-    RoleDTO edit(RoleDTO role);
-    RoleDTO update(RoleDTO role);
-    void delete(RoleDTO role);
-    void deleteById(Long id);
-*/
+
 
 }
